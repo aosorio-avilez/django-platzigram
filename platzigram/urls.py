0ba6c64574c802e71fb551f87487d8ef89945c5d
Alfonso Osorio Avilez <aosorio@plenumsoft.com.mx>
2019-04-16 18:33:07 -0500

    Visualizando imagenes y configurando el modelo post

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -2,6 +2,8 @@
 # Django
 from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from platzigram import views as local_views
 from posts import views as posts_views
@@ -13,4 +15,5 @@ urlpatterns = [
     path('hello-world/', local_views.hello_world),
     path('hi/', local_views.hi),
     path('posts/', posts_views.list_posts)
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
 
-# Register your models here.
+# Django
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Posts Admin model"""
+
+    list_display = ('pk', 'user', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('photo',)
+    list_filter = (
+                'created',
+                'modified'
+    )
